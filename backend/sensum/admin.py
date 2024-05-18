c16cd922c4ac6bc5effff343757a64f097486736
Eric Lew <eric@its-be-studio.com>
2024-05-18 16:54:30 +0100

    Added Event inline form to Course admin

--- backend/sensum/admin.py
+++ backend/sensum/admin.py
@@ -2,8 +2,31 @@ from django.contrib import admin
 
 from .models import Course, Lecturer, Event, Seat, Offer, Sponsor, CourseAdditionalInfo
 
+class EventInline(admin.TabularInline):
+  model = Event
+
+class CourseAdmin(admin.ModelAdmin):
+  fieldSets = (
+    ("None", {
+      "fields": (
+        "title",
+        "primary_subtitle",
+        "secondary_subtitle",
+        "duration",
+        "audience_groups",
+        "lecturers",
+        "sponsors",
+        "about_description",
+        "additional_info"
+      )
+    }),
+  )
+  inlines = [
+    EventInline
+  ]
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register([
-  Course,
   Lecturer,
   Event,
   Seat,
