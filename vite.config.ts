cd0c90f183ddce107b41c0b302e869bf3834b680
Eric Lew <eric@its-be-studio.com>
2024-08-04 11:13:10 +0300

    Frontend fixes & Specified Node frontend image version

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    watch: {
+      usePolling: true
+    }
   },
   test: {
     globals: true,
