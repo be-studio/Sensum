7398fbe5383955bc48916d81cefd79476aa4454c
Eric Lew <eric@its-be-studio.com>
2024-08-26 19:24:05 +0300

    Course model test

--- backend/sensum/tests/models/test_course.py
+++ backend/sensum/tests/models/test_course.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from django.db import DataError, IntegrityError
 from datetime import timedelta
 from ...models import Course, Group, Lecturer, Sponsor, CourseAdditionalInfo
+from ...utility.testing_utils import createRegex
 
 
 class CourseTestCase(TestCase):
@@ -8,23 +10,31 @@ class CourseTestCase(TestCase):
   def setUpClass(cls):
     super().setUpClass()
 
-    audience_group = Group.objects.create(
+    Group.objects.create(
       name="Visitors"
     )
-    lecturer = Lecturer.objects.create(
+    Lecturer.objects.create(
       name="Jane Brown"
     )
-    sponsor = Sponsor.objects.create(
+    Sponsor.objects.create(
       name="Toothpaste"
     )
-    additional_info = CourseAdditionalInfo.objects.create(
+    CourseAdditionalInfo.objects.create(
       title="Extra",
       description="More Info"
     )
+
+  @staticmethod
+  def create_course(title="Lorem Ipsum Dolor", primary_subtitle="Hello World", secondary_subtitle="Foo Bar"):
+    audience_group = Group.objects.get(name="Visitors")
+    lecturer = Lecturer.objects.get(name="Jane Brown")
+    sponsor = Sponsor.objects.get(name="Toothpaste")
+    additional_info = CourseAdditionalInfo.objects.get(title="Extra")
+
     course = Course.objects.create(
-      title="Lorem Ipsum Dolor",
-      primary_subtitle="Hello World",
-      secondary_subtitle="Foo Bar",
+      title=title,
+      primary_subtitle=primary_subtitle,
+      secondary_subtitle=secondary_subtitle,
       duration=timedelta(hours=1),
       about_description="This is a course for everyone",
       additional_info=additional_info
@@ -33,8 +43,11 @@ class CourseTestCase(TestCase):
     course.lecturers.add(lecturer)
     course.sponsors.add(sponsor)
 
+    return course
+
   def test_course_created(self):
-    course = Course.objects.get(title="Lorem Ipsum Dolor")
+    course = self.create_course()
+
     self.assertEqual(course.primary_subtitle, "Hello World")
     self.assertEqual(course.secondary_subtitle, "Foo Bar")
     self.assertEqual(course.duration, timedelta(hours=1))
@@ -44,6 +57,29 @@ class CourseTestCase(TestCase):
     self.assertEqual(course.about_description, "This is a course for everyone")
     self.assertEqual(course.additional_info, CourseAdditionalInfo.objects.get(title="Extra"))
 
+  def test_unique_course_title(self):
+    with self.assertRaisesRegex(IntegrityError, createRegex("duplicate entry")):
+      for _ in range(2):
+        self.create_course()
+
+  def test_valid_course_title_length(self):
+    with self.assertRaisesRegex(DataError, createRegex("data too long")):
+      self.create_course("x" * 121)
+
+  def test_valid_course_primary_subtitle_length(self):
+    with self.assertRaisesRegex(DataError, createRegex("data too long")):
+      self.create_course(primary_subtitle="x" * 501)
+
+  def test_valid_course_secondary_subtitle_length(self):
+    with self.assertRaisesRegex(DataError, createRegex("data too long")):
+      self.create_course(secondary_subtitle="x" * 501)
+
+  def test_course_audience_groups_verbose_name(self):
+    course = self.create_course()
+
+    self.assertEqual(course._meta.get_field("audience_groups").verbose_name, "Audience Groups")
+
   def test_get_default_string(self):
-    course = Course.objects.get(title="Lorem Ipsum Dolor")
+    course = self.create_course()
+
     self.assertEqual(course.__str__(), "Lorem Ipsum Dolor")
