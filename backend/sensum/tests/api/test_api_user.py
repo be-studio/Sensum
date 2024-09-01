ce8931adabd83b7c5078d67c9ba8644709a1820d
Eric Lew <eric@its-be-studio.com>
2024-09-01 11:43:58 +0300

    Backend test files lint

--- backend/sensum/tests/api/test_api_user.py
+++ backend/sensum/tests/api/test_api_user.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 from django.test import Client, TestCase
 
+
 class UserApiTestCase(TestCase):
   @classmethod
   def setUpClass(cls):
@@ -25,18 +26,20 @@ class UserApiTestCase(TestCase):
     c.force_login(user=user.objects.get(first_name="John"))
     response = c.get("/users")
 
-    self.assertEqual(response.json(), [
-      {
-        "first_name": "John",
-        "last_name": "Doe",
-        "email": "john.doe@email.com"
-      },
-      {
-        "first_name": "Jane",
-        "last_name": "Brown",
-        "email": "jane.brown@email.com"
-      }
-    ])
+    self.assertEqual(
+      response.json(), [
+        {
+          "first_name": "John",
+          "last_name": "Doe",
+          "email": "john.doe@email.com"
+        },
+        {
+          "first_name": "Jane",
+          "last_name": "Brown",
+          "email": "jane.brown@email.com"
+        }
+      ]
+    )
 
   def test_get_user(self):
     user = get_user_model()
@@ -44,9 +47,11 @@ class UserApiTestCase(TestCase):
     c.force_authenticate(user=user.objects.get(first_name="John"))
     response = c.get("/api/user/1")
 
-    self.assertEqual(response.json(), {
-      "id": 1,
-      "first_name": "John",
-      "last_name": "Doe",
-      "email": "john.doe@email.com"
-    })
+    self.assertEqual(
+      response.json(), {
+        "id": 1,
+        "first_name": "John",
+        "last_name": "Doe",
+        "email": "john.doe@email.com"
+      }
+    )
